f7c683fa2be4bafafd4cae415d56707543ad82eb
owl <j.h4nsen@gmail.com>
2020-08-10 17:49:34 +0200

    fixed STORE source reg to rs2

--- riscv_memory.c
+++ riscv_memory.c
@@ -4,6 +4,7 @@
 #define _mem_addr state->mem_addr
 #define _mem_size state->mem_size
 #define _rd_dat state->rd_dat
+#define _rs2_dat state->rs2_dat
 
 static void write_mem_word(EmulatorState* state)
 {
@@ -13,7 +14,7 @@ static void write_mem_word(EmulatorState* state)
         return;
     }
     uint32_t *int_ptr = (uint32_t*) _mem + _mem_addr;
-    *int_ptr = _rd_dat;
+    *int_ptr = _rs2_dat;
     return;
 }
 
@@ -25,7 +26,7 @@ static void write_mem_half(EmulatorState* state)
         return;
     }
     uint16_t *half_ptr = (uint16_t*) _mem + _mem_addr;
-    *half_ptr = (uint16_t) _rd_dat;
+    *half_ptr = (uint16_t) _rs2_dat;
     return;
 }
 
@@ -37,7 +38,7 @@ static void write_mem_byte(EmulatorState* state)
         return;
     }
     uint8_t *byte_ptr = (uint8_t*) _mem + _mem_addr;
-    *byte_ptr = (uint8_t) _rd_dat;
+    *byte_ptr = (uint8_t) _rs2_dat;
     return;
 }
 
